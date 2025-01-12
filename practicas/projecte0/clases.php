fd23fadef5b97aa9c5f9765c20a3f4c03677d7f5
NouriHicham <156692558+NouriHicham@users.noreply.github.com>
2025-01-12 12:57:32 +0000

    projecte0 casi terminado

---
+++ practicas/projecte0/clases.php
@@ -0,0 +1,66 @@
+<?php
+session_start();
+
+class Libro
+{
+   public string $titol;
+   public string $autor;
+   public int $anyPublic;
+   public string $foto;
+
+   public function __construct($titol, $autor, $anyPublic, $foto)
+   {
+      $this->titol = $titol;
+      $this->autor = $autor;
+      $this->anyPublic = $anyPublic;
+      $this->foto = $foto;
+   }
+
+   public function detalls()
+   {
+      return '
+         <div class="card me-3" style="width: 18rem;">
+            <img src="'.$this->foto.'" class="card-img-top">
+            <div class="card-body">
+               <h5 class="card-title">'.$this->titol.'</h5>
+               <p class="card-text">'.$this->autor.' - '.$this->anyPublic.'</p>
+            </div>
+         </div>
+      ';
+   }
+}
+
+class Biblioteca
+{
+   public array $biblioteca = [];
+
+   public function afegirLlibre($titol, $autor, $anyPublic, $foto){
+      $libro = new Libro($titol, $autor, $anyPublic, $foto);
+      array_push($this->biblioteca, $libro);
+   }
+
+   public function mostrarLlibres(){
+      foreach ($this->biblioteca as $libro) {
+         echo $libro->detalls();
+      }
+   }
+
+   public function buscarLlibre(string $busqueda){
+      foreach ($this->biblioteca as $libro) {
+         //preguntar profe el !==false
+         if(strpos(strtolower($libro->titol), strtolower($busqueda)) !== false){
+            return '
+                     <div class="card me-3" style="width: 18rem;">
+                        <img src="'.$libro->foto.'" class="card-img-top">
+                        <div class="card-body">
+                           <h5 class="card-title">'.$libro->titol.'</h5>
+                           <p class="card-text">'.$libro->autor.' - '.$libro->anyPublic.'</p>
+                        </div>
+                     </div>
+                   ';
+            
+            
+         }
+      }
+   }
+}
--- practicas/projecte0/index.php
+++ practicas/projecte0/index.php
@@ -1,47 +1,77 @@
 <?php
-session_start();
+   session_start();
+   include_once 'clases.php';
 
-class Libro
-{
-   public string $titol;
-   public string $autor;
-   public int $anyPublic;
-   public string $foto;
+   $_SESSION['prueba'] = new Biblioteca;
+   $_SESSION['prueba']->afegirLlibre('Cien años de soledad', 'Gabriel García Márquez', 1967, 'https://m.media-amazon.com/images/I/91TvVQS7loL._AC_UF894,1000_QL80_.jpg');
+   $_SESSION['prueba']->afegirLlibre('Mil soles espléndidos', 'Khaled Hosseini', 2007, 'https://m.media-amazon.com/images/I/71dGXojweAL._UF1000,1000_QL80_.jpg');
+   $_SESSION['prueba']->afegirLlibre('El principito', 'Antoine de Saint-Exupéry', 1943, 'https://m.media-amazon.com/images/I/714Hvb52n-L._AC_UF894,1000_QL80_.jpg');
 
-   public function __construct($titol, $autor, $anyPublic, $foto)
-   {
-      $this->titol = $titol;
-      $this->autor = $autor;
-      $this->anyPublic = $anyPublic;
-      $this->foto = $foto;
-   }
+?>
 
-   public function detalls()
-   {
-      return 'El llibre ' . $this->titol . ' de ' . $this->autor . ' publicat el ' . $this->anyPublic . '';
-   }
-}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Document</title>
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+   <div class="container">
+      <a href="logout.php" class="btn btn-danger mt-3">Reiniciar</a>
+      <div class="row">
+         <div class="col">
+            <h2>Afegir llibre</h2>
+            
+            <form method="get">
+               <div class="mb-3">
+                  <label for="titol" class="form-label">Títol</label>
+                  <input type="text" class="form-text" id="titol" name="titol" value="1984">
+               </div>
+               <div class="mb-3">
+                  <label for="autor" class="form-label">Autor</label>
+                  <input type="text" class="form-text" id="autor" name="autor" value="George Orwell">
+               </div>
+               <div class="mb-3">
+                  <label for="any" class="form-label">Any publicació</label>
+                  <input type="number" class="form-text" id="any" name="any" value="1949">
+               </div>
+               <div class="mb-3">
+                  <label for="imatge" class="form-label">Url imatge</label>
+                  <input type="text" class="form-text" id="imatge" name="imatge" value="https://m.media-amazon.com/images/I/71sOSrd+JxL._AC_UF894,1000_QL80_.jpg">
+               </div>
+               <button type="submit" class="btn btn-primary" name="tipo" value="afegir">Afegir</button>
+            </form>
+         </div>
+         <div class="col">
+            <h2>Buscar llibre</h2>
+            <form method="get">
+               <div class="mb-3">
+                  <label for="titol" class="form-label">Títol</label>
+                  <input type="text" class="form-text" id="titol" name="titol">
+               </div>
+               <button type="submit" class="btn btn-primary" name="tipo" value="buscar">Buscar</button>
+            </form>
+         </div>
+      </div>
+      <h3 class="mt-3">Llibres</h3>
+      <div class="d-flex flex-wrap">
+         <?php 
+            if($_GET['tipo']=='afegir'){
+               $_SESSION['prueba']->afegirLlibre($_GET['titol'],$_GET['autor'],$_GET['any'],$_GET['imatge']);
+               $_SESSION['prueba']->mostrarLlibres(); 
+            }else if($_GET['tipo']=='buscar'){
+               echo $_SESSION['prueba']->buscarLlibre($_GET['titol']);
+            }else{
+               $_SESSION['prueba']->mostrarLlibres(); 
+            }
+            
+         ?>
+      </div>
+      
+   </div>
 
-class Biblioteca
-{
-   public array $biblioteca = [];
-
-   //funcio que al afegir un llibre al array li afegira un numero endevant segons el numero de elements
-   public function afegirLlibre($titol, $autor, $anyPublic, $foto){
-      $libro = new Libro($titol, $autor, $anyPublic, $foto);
-      array_push($this->biblioteca, $libro);
-   }
-
-   public function mostrarLlibres(){
-      foreach ($this->biblioteca as $libro) {
-         
-         echo $libro->detalls();
-      }
-   }
-}
-
-$prueba = new Biblioteca;
-$prueba->afegirLlibre('Cien años de soledad', 'Gabriel García Márquez', 1967, 'https://m.media-amazon.com/images/I/91TvVQS7loL._AC_UF894,1000_QL80_.jpg');
-$prueba->afegirLlibre('aaa', 'bbb', 1923, 'ccc');
-
-echo $prueba->mostrarLlibres();
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
